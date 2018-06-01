2e3a3740a4e546017bfee35212529e6cd0e272f4
Milko Škofič <skofic@gmail.com>
2018-06-01 17:34:59 +0200

    Added deletion check in user test.
    Need to remove redundant test services.
    Must finalise forms.

--- routes/test/User.js
+++ routes/test/User.js
@@ -173,7 +173,45 @@ router.post
 			//
 			let remove = null;
 			if( request.body.remove )
+			{
+				//
+				// Remove user.
+				//
 				remove = user.remove();
+				
+				//
+				// Validate removal.
+				//
+				let cursor;
+				const selector = {};
+				
+				//
+				// Check user record.
+				//
+				selector._id = id;
+				cursor = db._collection( 'schemas' ).byExample( selector );
+				if( cursor.count() > 0 )
+					theResponse.throw( 500, "User not deleted." );
+				delete selector._id;
+				
+				//
+				// Check manager.
+				//
+				selector[ Dict.descriptor.kPredicate ] = Dict.term.kPredicateManagedBy;
+				selector._from = id;
+				cursor = db._collection( 'schemas' ).byExample( selector );
+				if( cursor.count() > 0 )
+					theResponse.throw( 500, "Manager edge not deleted." );
+				delete selector._from;
+				
+				//
+				// Check managed.
+				//
+				selector._to = id;
+				cursor = db._collection( 'schemas' ).byExample( selector );
+				if( cursor.count() > 0 )
+					theResponse.throw( 500, "Managed edge not deleted." );
+			}
 			
 			response.send({
 				params : request.body,
@@ -261,345 +299,6 @@ router.post
 	)
 	.description(dd`
   Instantiates and returns a user object
-`);
-
-
-/**
- * Test user creation
- *
- * The service will create a user and return its data, it expects three parameters
- * in the POST body:
- *
- * 	- user:		Either a user object.
- * 	- group:	The optional user group object or reference.
- * 	- manager:	The optional user manager object or reference.
- *
- * @path		/instantiate
- * @verb		post
- * @response	{Object}	The operation result.
- */
-router.post
-(
-	'/create',
-	(request, response) =>
-	{
-		//
-		// Init timer.
-		//
-		const stamp = time();
-		
-		//
-		// Test creation.
-		//
-		try
-		{
-			//
-			// Resolve manager.
-			//
-			const manager = ( request.body.hasOwnProperty( 'manager' ) )
-						 ? new User( request, request.body.manager )
-						 : new User( request, request.session.uid );
-			if( ! manager.persistent )
-				manager.resolve( false, true );
-			
-			//
-			// Instantiate user.
-			//
-			const user = new User( request, request.body.user, null, manager.document._id );
-			
-			//
-			// Create authorisation data.
-			//
-			const data = {};
-			const auth = createAuth();
-			data[ Dict.descriptor.kAuthData ] =
-				auth.create( "secret" );
-			user.modify( data, true, false );
-			
-			//
-			// Insert user.
-			//
-			user.insert();
-			
-			response.send({
-				user : user.document,
-				time : time() - stamp
-			});
-		}
-		catch( error )
-		{
-			//
-			// Init local storage.
-			//
-			let http = 500;
-			
-			//
-			// Handle MyError exceptions.
-			//
-			if( (error.constructor.name === 'MyError')
-			 && error.hasOwnProperty( 'param_http' ) )
-				http = error.param_http;
-			
-			response.throw( http, error );										// !@! ==>
-		}
-	},
-	'create'
-)
-	.body(
-		Joi.object({
-			user : Joi.alternatives().try(
-				Joi.string().required(),
-				Joi.object().required()
-			).required(),
-			group : Joi.alternatives().try(
-				Joi.string(),
-				null
-			),
-			manager : Joi.alternatives().try(
-				Joi.string().required(),
-				Joi.object().required(),
-				null
-			).required()
-		}).required(),
-		"An object with 'user' that contains eother the user structure or a string" +
-		" representing the user reference, 'group' containing the user group reference" +
-		" and 'manager' containing the eventual user manager reference."
-	)
-	.response(
-		200,
-		Joi.object(),
-		"The result: 'user' contains the user object, 'group' con tains the" +
-		" group reference, 'manager' contains the eventual user manager object " +
-		"and 'time' contains the elapsed time."
-	)
-	.summary(
-		"Instantiate a user object."
-	)
-	.description(dd`
-  Instantiates and returns a user object
-`);
-
-
-/**
- * Test user replacement
- *
- * The service will instantiate a user from a document and replace the existing
- * document if it exists, it expects two parameters in the POST body:
- *
- * 	- existing:	The reference or object of the existing user.
- * 	- replaced:	The object of the replacing user.
- *
- * @path		/replace
- * @verb		post
- * @response	{Object}	The operation result.
- */
-router.post
-(
-	'/replace',
-	(request, response) =>
-	{
-		//
-		// Init timer.
-		//
-		const stamp = time();
-		
-		//
-		// Get parameters.
-		//
-		const usr = request.body.existing;
-		
-		//
-		// Test instantiation.
-		//
-		try
-		{
-			//
-			// Instantiate user.
-			//
-			const user = new User( request, usr );
-			
-			//
-			// Resolve user.
-			//
-			if( ! user.persistent )
-				user.resolve( false, true );
-			
-			//
-			// Update user.
-			//
-			for( const field in request.body.replaced )
-				user._document[ field ] = request.body.replaced[ field ];
-			
-			//
-			// Replace user.
-			//
-			const checkRevision = true;
-			const replaced = user.replace( checkRevision );
-			
-			response.send({
-				replaced : replaced,
-				group : user.group,
-				manager : user.manager,
-				collection : user.collection,
-				class : user.classname,
-				persistent : user.persistent,
-				revised : user.revised,
-				hasManaged : user.hasManaged(),
-				managed : user.managed,
-				user : user.document,
-				time : time() - stamp
-			});
-		}
-		catch( error )
-		{
-			//
-			// Init local storage.
-			//
-			let http = 500;
-			
-			//
-			// Handle MyError exceptions.
-			//
-			if( (error.constructor.name === 'MyError')
-			 && error.hasOwnProperty( 'param_http' ) )
-				http = error.param_http;
-			
-			response.throw( http, error );										// !@! ==>
-		}
-	},
-	'replace'
-)
-	.body(
-		Joi.object({
-			existing : Joi.alternatives().try(
-				Joi.string().required(),
-				Joi.object().required()
-			).required(),
-			replaced : Joi.object()
-		}).required(),
-		"The existing and replacement users."
-	)
-	.response(
-		200,
-		Joi.object(),
-		"An object containing relevant information."
-	)
-	.summary(
-		"Replace a user object."
-	)
-	.description(dd`
-  Replace an existing user
-`);
-
-
-/**
- * Test user deletion
- *
- * The service will instantiate a user from a reference and delete the user if it
- * exists, it expects a single parameter in the POST body:
- *
- * 	- reference:	The user reference or an object containing the user code.
- *
- * @path		/delete
- * @verb		post
- * @response	{Object}	The operation result.
- */
-router.post
-(
-	'/delete',
-	(request, response) =>
-	{
-		//
-		// Init timer.
-		//
-		const stamp = time();
-		
-		//
-		// Get parameters.
-		//
-		const usr = request.body.reference;
-		
-		//
-		// Test instantiation.
-		//
-		try
-		{
-			//
-			// Instantiate user.
-			//
-			const user = new User( request, usr );
-			
-			//
-			// Resolve user.
-			//
-			const doReplace = false;
-			const doAssert  = false;
-			if( ! user.persistent )
-				user.resolve( doReplace, doAssert );
-			
-			//
-			// Insert user.
-			//
-			let deleted = false;
-			if( user.persistent )
-			{
-				user.remove();
-				deleted = true;
-			}
-			
-			response.send({
-				deleted : deleted,
-				group : user.group,
-				manager : user.manager,
-				collection : user.collection,
-				class : user.classname,
-				persistent : user.persistent,
-				revised : user.revised,
-				hasManaged : user.hasManaged(),
-				managed : user.managed,
-				user : user.document,
-				time : time() - stamp
-			});
-		}
-		catch( error )
-		{
-			//
-			// Init local storage.
-			//
-			let http = 500;
-			
-			//
-			// Handle MyError exceptions.
-			//
-			if( (error.constructor.name === 'MyError')
-			 && error.hasOwnProperty( 'param_http' ) )
-				http = error.param_http;
-			
-			response.throw( http, error );										// !@! ==>
-		}
-	},
-	'delete'
-)
-	.body(
-		Joi.object({
-			reference : Joi.alternatives().try(
-				Joi.string().required(),
-				Joi.object().required()
-			).required()
-		}).required(),
-		"An object with 'reference' that contains eother the user structure or a string" +
-		" representing the user reference."
-	)
-	.response(
-		200,
-		Joi.object(),
-		"An object containing relevant information."
-	)
-	.summary(
-		"Delete a user object."
-	)
-	.description(dd`
-  Delete an existing user
 `);
 
 
