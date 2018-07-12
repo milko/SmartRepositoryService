b4ca4ac990e59288b6299efcad694ff9787d2632
Milko Škofič <skofic@gmail.com>
2018-07-12 12:40:35 +0200

    Finalised Toponym class.
    Not tested yet.

---
+++ classes/Toponym.js
@@ -0,0 +1,65 @@
+'use strict';
+
+//
+// Parent.
+//
+const Term = require( './Term' );
+
+
+/**
+ * Toponym class
+ *
+ * This class extends the Term class by setting the default collection and instance.
+ */
+class Toponym extends Term
+{
+	/**
+	 * Init document properties
+	 *
+	 * We overload this method to set the instance member.
+	 *
+	 * @param theRequest	{Object}			The current request.
+	 * @param theCollection	{String}|{null}		The document collection.
+	 * @param isImmutable	{Boolean}			True, instantiate immutable document.
+	 */
+	initDocumentMembers( theRequest, theCollection, isImmutable )
+	{
+		//
+		// Call parent method.
+		//
+		super.initDocumentMembers( theRequest, theCollection, isImmutable );
+		
+		//
+		// Set edge instance.
+		//
+		this._instance = 'Toponym';
+		
+	}	// initDocumentMembers
+	
+	
+	/************************************************************************************
+	 * DEFAULT GLOBALS																	*
+	 ************************************************************************************/
+	
+	/**
+	 * Return default collection name
+	 *
+	 * This method should return the default collection name: if documents of this
+	 * class belong to a specific collection, this method should return its name; if
+	 * documents of this class may be stored in different collectons, this method
+	 * should return null.
+	 *
+	 * We overload this method to return the terms collection.
+	 *
+	 * @returns {String}|{null}	The default collection name.
+	 */
+	get defaultCollection()
+	{
+		return 'toponyms';															// ==>
+		
+	}	// defaultCollection
+	
+	
+}	// Toponym.
+
+module.exports = Toponym;
